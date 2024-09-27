8d130c62f52eb2bdf45c80bbf5a29026831cf6da
M.Kozyukov <kozukovmisa@gmail.com>
2024-09-27 08:36:36 +0300

    ''

--- base/views.py
+++ base/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.contrib.auth import logout
+from .models import ContentHtmlMain
 
 
 # Create your views here.
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'base.html')
+    content = ContentHtmlMain.objects.all()
+    context = {
+        'content': content
+    }
+    return render(request, 'home.html', context)
 
 
 def custom_logout(request):
