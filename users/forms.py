5bd8c648dfb2a7fb38d9f5a1f8b49628558d6aba
M.Kozyukov <kozukovmisa@gmail.com>
2024-10-15 09:13:43 +0300

    ''

--- users/forms.py
+++ users/forms.py
@@ -4,8 +4,14 @@ from django.contrib.auth.models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(label='Email', required=True)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        labels = {
+            'username': 'Username',
+            'password1': 'Password',
+            'password2': 'Confirm Password',
+        }
+        
